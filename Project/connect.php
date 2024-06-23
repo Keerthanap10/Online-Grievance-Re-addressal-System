e8d204093b5fad1f1443794d79f934ce0640b2a0
Keerthanap10 <164132541+Keerthanap10@users.noreply.github.com>
2024-06-23 18:05:37 +0530

    Update connect.php

--- Project/connect.php
+++ Project/connect.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$username = 'wadprj';
-$password = 'KeerthanaE@1009';
-$database = 'wadprj';
+$username = '/*Your username*/';
+$password = '/*Your password*/';
+$database = 'wadprj'; //my database name
 $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
